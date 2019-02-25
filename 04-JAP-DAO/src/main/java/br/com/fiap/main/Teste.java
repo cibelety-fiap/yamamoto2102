c89ae88018fc4a568408c7b6865caabb50ed764b
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-25 09:02:14 -0300

    teste

--- 04-JAP-DAO/src/main/java/br/com/fiap/main/Teste.java
+++ 04-JAP-DAO/src/main/java/br/com/fiap/main/Teste.java
@@ -11,10 +11,12 @@ import br.com.fiap.dao.EmpresaDAO;
 import br.com.fiap.dao.impl.EmpresaDAOImpl;
 import br.com.fiap.entity.Empresa;
 import br.com.fiap.entity.Status;
+import br.com.fiap.exception.CodigoInexistenteException;
+import br.com.fiap.exception.CommitException;
 
 public class Teste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CommitException {
 
 		EntityManagerFactory fabrica = 
 				Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
@@ -24,8 +26,38 @@ public class Teste {
 		
 		
 		//Cadastrar uma Empresa
-		dao.cadastrar(new Empresa("ABC", new GregorianCalendar(1990, Calendar.JANUARY,1), Status.ATIVO, "123/001"));
 		
+		try {
+			dao.cadastrar(new Empresa("ABC", new GregorianCalendar(1990, Calendar.JANUARY,1), Status.ATIVO, "123/001"));
+			dao.commit();
+			System.out.println("Cadastrado com sucesso!");
+		} catch (CommitException e) {
+			e.printStackTrace();
+			System.err.println("Erro ao cadastrar");
+		}
+		
+		System.out.println(dao.pesquisar(1).getNomeFantasia());
+		
+		try {
+			dao.atualizar(new Empresa(1, "BCD", new GregorianCalendar(2000, Calendar.JANUARY,1), Status.ATIVO, "123/001"));
+			dao.commit();
+			System.out.println("Atualizado com sucesso!");
+		} catch (CommitException e) {
+			e.printStackTrace();
+			System.err.println("Erro ao atualizar");
+		}
+		
+		System.out.println(dao.pesquisar(1).getNomeFantasia());
+/*		
+		try {
+			dao.excluir(1);
+			dao.commit();
+			System.out.println("Excluído com sucesso!");
+		} catch (CodigoInexistenteException e) {
+			e.printStackTrace();
+			System.err.println("Erro ao excluir");
+		}
+*/		
 		em.close();
 		fabrica.close();
 	}

58ade17911364e7c66502f47718aa5d568e691f8
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-14 11:29:01 -0300

    asdf

--- 05-JPA-DesignPatterns/src/br/com/fiap/teste/ProfessorDAOTeste.java
+++ 05-JPA-DesignPatterns/src/br/com/fiap/teste/ProfessorDAOTeste.java
@@ -2,7 +2,10 @@ package br.com.fiap.teste;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import javax.persistence.EntityManager;
@@ -10,6 +13,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.fiap.dao.ProfessorDAO;
@@ -22,6 +26,8 @@ class ProfessorDAOTeste {
 	
 	private static ProfessorDAO dao;
 	
+	private Professor prof;
+	
 	//método que será executado antes de todos os testes
 	@BeforeAll
 	public static void inicializar() {
@@ -30,11 +36,10 @@ class ProfessorDAOTeste {
 		dao = new ProfessorDAOImpl(em);
 	}
 	
-
-	@Test
-	void cadastroTest() {
+	@BeforeEach
+	public void antesTest() {
 		// Arrange - instanciar os objetos
-		Professor prof = new Professor("Parducci", null, "123456789");
+		prof = new Professor("Parducci", null, "123456789");
 		
 		// Act
 		try {
@@ -45,32 +50,88 @@ class ProfessorDAOTeste {
 			fail("Falha no teste...");
 
 		}
+	}
+	
 
+	@Test
+	void cadastroTest() {
 		// Assert - valida o resultado
 		// valida se foi gerado um código pela sequence
 		assertNotEquals(0, prof.getCodigo());
 	}
 	
 	@Test
+	void removerTest() throws CommitException {
+		
+		try {
+			dao.excluir(prof.getCodigo());
+			dao.commit();	
+		} catch (CodigoInexistenteException e) {
+			e.printStackTrace();
+			fail("Falha no teste...");
+		}	
+		
+		assertThrows(CodigoInexistenteException.class, ()->dao.pesquisar(prof.getCodigo()));
+		
+	}
+	
+	@Test
+	void pesquisarTest() {
+		try {
+			
+			Professor busca = dao.pesquisar(prof.getCodigo());
+			
+			assertNotNull(busca);
+			assertEquals(busca.getNome(), prof.getNome());
+			
+		} catch (CodigoInexistenteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Falha no teste...");
+		}
+	}
+	
+	
+	@Test
 	void atualizarTest() {
 		// Arrange - instanciar os objetos
-		Professor prof = new Professor(1, "Blabla", null, "987654321");
+		Professor prof = new Professor("Blabla", null, "987654321");
 
 		//Act
+		// Cadastrar um professor
 		try {	
-			dao.atualizar(prof);
+			dao.cadastrar(prof);
 			dao.commit();
 		} catch (CommitException e) {
 			e.printStackTrace();
 			fail("Falha no teste...");
 		}
 
+		// Atualizar o professor
+		
+		Professor prof2 = new Professor(prof.getCodigo(), "Rafael", null, "123123123");
+		
+		try {	
+			dao.atualizar(prof2);
+			dao.commit();
+		} catch (CommitException e) {
+			e.printStackTrace();
+			fail("Falha no teste...");
+		}
+		
+		//Assert
+		//Pesquisa e valida se o valor foi alterado no banco
+		
+		
 		try {
-			assertEquals(prof.getNome(), dao.pesquisar(prof.getCodigo()).getNome());
+			Professor prof3 = dao.pesquisar(prof.getCodigo());
+			assertEquals(prof2.getNome(), prof3.getNome());
+			assertEquals(prof2.getCpf(), prof3.getCpf());
+					
 		} catch (CodigoInexistenteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
+		}	
 	}
+	
 }
